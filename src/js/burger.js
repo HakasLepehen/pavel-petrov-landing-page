b92de963e0ab5818bcfa566b2c43dd223fdfe785
Pavel Petrov <doctorrosen2016@gmail.com>
2021-09-19 23:39:30 +0600

    working with adaptive navbar

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,11 +1,12 @@
-let menuBtn = document.querySelector('.header__burger');
-let menu = document.querySelector('.header__menu');
+let menuBtn = document.querySelector(".header__burger");
+let menu = document.querySelector(".header__menu");
 
-function openMenu() {
+function openMenu() {}
 
-}
-
-menuBtn.addEventListener('click', () => {
-    menuBtn.classList.toggle('active');
-    menu.classList.toggle('active');
-})
+menuBtn.addEventListener("click", () => {
+  menuBtn.classList.toggle("active");
+  menu.classList.toggle("active");
+  menu.classList.contains("active")
+    ? (menu.style.display = "block")
+    : (menu.style.display = "none");
+});
