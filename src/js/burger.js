22aea6a0321f8c6f4dc55221f22771f056b01d75
Pavel Petrov <doctorrosen2016@gmail.com>
2021-09-21 01:07:57 +0600

    working with greetings section

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,12 +1,13 @@
 let menuBtn = document.querySelector(".header__burger");
 let menu = document.querySelector(".header__menu");
 
-function openMenu() {}
-
 menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("active");
   menu.classList.toggle("active");
+
   menu.classList.contains("active")
     ? (menu.style.display = "block")
     : (menu.style.display = "none");
+
+  document.getElementsByTagName("body")[0].style.overflow = "hidden";
 });
