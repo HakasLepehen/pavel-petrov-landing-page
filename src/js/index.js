67f97a40beed22e9191cb7807b08e83c0a68c826
Pavel Petrov <doctorrosen2016@gmail.com>
2021-09-24 22:56:08 +0600

    fix bug with image in safari

--- src/js/index.js
+++ src/js/index.js
@@ -4,4 +4,5 @@ import '../fonts/Heebo/heebo.woff';
 import '../fonts/Heebo/heebo.woff2';
 import './burger.js';
 import '../img/avatar.webp';
+import '../img/avatar.jpeg';
 import './experience-slider-script.js';
