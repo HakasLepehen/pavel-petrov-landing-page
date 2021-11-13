f6f0703f095aa661309c61f450887a872772ac0d
Tanuj Kanti <86398394+Tanujkanti4441@users.noreply.github.com>
2021-11-12 22:47:18 -0800

    fixes nav js

--- src/js/burger.js
+++ src/js/burger.js
@@ -5,9 +5,9 @@ menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("active");
   menu.classList.toggle("active");
 
-  menu.classList.contains("active")
-    ? (menu.style.display = "block")
-    : (menu.style.display = "none");
+//   menu.classList.contains("active")
+//     ? (menu.style.display = "block")
+//     : (menu.style.display = "none");
 
   document.getElementsByTagName("body")[0].style.overflow = "hidden";
 });
